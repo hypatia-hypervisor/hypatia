d612e67f526e2c2ce933b1df309db6ea0a605563
Dan Cross <cross@gajendra.net>
2023-02-06 15:49:07 +0000

    global: prototype transfer vectors
    Add a prototype transfer vector into `global`.
    
    Signed-off-by: Dan Cross <cross@gajendra.net>

---
+++ global/src/x86_64/mod.rs
@@ -0,0 +1,8 @@
+// Copyright 2023  The Hypatia Authors
+// All rights reserved
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+mod xferv;
