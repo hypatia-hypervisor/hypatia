2bcce3a587ec5aa32ee9b4f8303dc234f8197f88
Dan Cross <cross@gajendra.net>
2023-02-06 15:53:28 +0000

    WIP: Laying the groundwork for per-node/CPU segments
    Signed-off-by: Dan Cross <cross@gajendra.net>

---
+++ node/src/main.rs
@@ -0,0 +1,32 @@
+// Copyright 2023  The Hypatia Authors
+// All rights reserved
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#![feature(naked_functions)]
+#![feature(strict_provenance)]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(not(test), no_std)]
+#![forbid(absolute_paths_not_starting_with_crate)]
+#![forbid(elided_lifetimes_in_paths)]
+#![forbid(unsafe_op_in_unsafe_fn)]
+
+use arch::Page4K;
+
+mod x86_64;
+
+/// Returns a static reference to the global zero page.
+pub fn zero_page() -> &'static Page4K {
+    const ZERO_PAGE: Page4K = Page4K::new();
+    &ZERO_PAGE
+}
+
+/// Initialize the system.
+#[no_mangle]
+pub extern "C" fn init(allocator: &dyn FnMut() -> Result<arch::Page4K, ()>) {
+    x86_64::init();
+}
+
+hypatia::runtime!();
---
+++ node/src/x86_64/xferv.rs
@@ -0,0 +1,31 @@
+// Copyright 2023  The Hypatia Authors
+// All rights reserved
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+use core::arch::asm;
+
+#[export_name = "xferv"]
+#[link_section = ".xferv"]
+#[naked]
+pub unsafe extern "C" fn xferv() {
+    unsafe {
+        asm!(r#"
+            .balign 8; jmp {hi};
+            .balign 8; jmp {bye};
+            "#,
+            hi = sym hi,
+            bye = sym bye,
+            options(att_syntax, noreturn));
+    }
+}
+
+pub extern "C" fn hi() {
+    uart::panic_println!("Hi!");
+}
+
+pub extern "C" fn bye() {
+    uart::panic_println!("Bye!");
+}
