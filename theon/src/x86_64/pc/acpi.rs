8c2002ccad67ed88b67e3c5104ff17da3fa956eb
Dan Cross <cross@gajendra.net>
2023-05-10 16:43:53 +0000

    acpi: parse tables
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- theon/src/x86_64/pc/acpi.rs
+++
@@ -1,6 +0,0 @@
-// Copyright 2022  The Hypatia Authors
-// All rights reserved
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
