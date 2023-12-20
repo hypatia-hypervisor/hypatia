3bdd7e0c90d9452881de2da17c060c5b1e732b33
Dan Cross <cross@gajendra.net>
2023-12-20 00:34:35 +0000

    vmx: start fleshing out Intel support
    Signed-off-by: Dan Cross <cross@gajendra.net>

---
+++ vcpu/src/vmx/mod.rs
@@ -0,0 +1,10 @@
+// Copyright 2023  The Hypatia Authors
+// All rights reserved
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+pub(crate) mod asm;
+pub(crate) mod vmcs;
+
---
+++ vcpu/src/vmx/vmcs.rs
@@ -0,0 +1,12 @@
+// Copyright 2023  The Hypatia Authors
+// All rights reserved
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#[repr(u64)]
+enum State {
+    Clear = 0,
+    Launched = 1,
+}
